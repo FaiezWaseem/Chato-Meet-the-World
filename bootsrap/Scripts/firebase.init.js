c8635e6985eefc2edf6f836d8e63d7c3281272dd
FaiezWaseem <78906502+FaiezWaseem@users.noreply.github.com>
2021-06-06 17:06:05 +0500

    script files

---
+++ bootsrap/Scripts/firebase.init.js
@@ -0,0 +1,34 @@
+﻿// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDNCHcEXWEfmydLleKIybu37LOo4ORR7B4",
+    authDomain: "chat-app2-b59ab.firebaseapp.com",
+    databaseURL: "https://chat-app2-b59ab.firebaseio.com",
+    projectId: "chat-app2-b59ab",
+    storageBucket: "chat-app2-b59ab.appspot.com",
+    messagingSenderId: "372802461916",
+    appId: "1:372802461916:web:e1dc92770ae3cc011c5419",
+    measurementId: "G-CWGH18RHPL"
+  };
+
+  firebase.initializeApp(firebaseConfig);  
+  
+
+
+  function signIn(){
+  var provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider );
+  }
+function signOut(){
+    firebase.auth().signOut();
+}
+function onfirebasestatechanged(){
+    firebase.auth().onAuthStateChanged(onStateChanged);
+}
+function onStateChanged(user){
+if(user){
+    // alert(firebase.auth().currentUser.email+'\n'+firebase.auth().currentUser.displayName);
+    document.getElementById('imageProfile').src = firebase.auth().currentUser.photoURL;
+    document.getElementById('imageProfile').title = firebase.auth().currentUser.displayName;
+}
+}
+onfirebasestatechanged();
