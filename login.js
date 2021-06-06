1e1d4cc0817e74e0b437849e9d9eee82588ae8c1
FaiezWaseem <78906502+FaiezWaseem@users.noreply.github.com>
2021-06-06 19:40:27 +0500

    fixing login info save problem

--- login.js
+++ login.js
@@ -37,7 +37,7 @@ function signUp(){
             alert("Signed Up");
          var fuserid = firebase.auth().currentUser.uid;
          console.log(fuserid);
-         firebase.database().ref("JAVASCRIPTLOGIN/").set({
+         firebase.database().ref("JAVASCRIPTLOGIN/"+fuserid).set({
              "uid": fuserid ,
              "Email": UserMail,
              "name": UserName,
